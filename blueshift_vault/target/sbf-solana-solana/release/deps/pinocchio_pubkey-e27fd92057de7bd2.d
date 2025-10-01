b2dd07a5da5eba74c8f574ce149952aeaa0412ab
Mishal Turkane <95625543+mishalturkane@users.noreply.github.com>
2025-10-01 09:16:35 +0530

    Delete blueshift_vault/target directory

--- blueshift_vault/target/sbf-solana-solana/release/deps/pinocchio_pubkey-e27fd92057de7bd2.d
+++
@@ -1,7 +0,0 @@
-/home/mishal/blueshift-challenges/blueshift_vault/target/sbf-solana-solana/release/deps/libpinocchio_pubkey-e27fd92057de7bd2.rmeta: /home/mishal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pinocchio-pubkey-0.3.0/src/lib.rs
-
-/home/mishal/blueshift-challenges/blueshift_vault/target/sbf-solana-solana/release/deps/libpinocchio_pubkey-e27fd92057de7bd2.rlib: /home/mishal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pinocchio-pubkey-0.3.0/src/lib.rs
-
-/home/mishal/blueshift-challenges/blueshift_vault/target/sbf-solana-solana/release/deps/pinocchio_pubkey-e27fd92057de7bd2.d: /home/mishal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pinocchio-pubkey-0.3.0/src/lib.rs
-
-/home/mishal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pinocchio-pubkey-0.3.0/src/lib.rs:
